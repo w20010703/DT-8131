3d84fc488172eba990bb86f28f941c84afe12ef0
jomasan <jomasan@gmail.com>
2025-08-27 11:14:25 -0400

    Website teamplate

---
+++ 01 html Template/script.js
@@ -0,0 +1,4 @@
+// Cornell Tech Design Tech Project Template
+// Clean template - no JavaScript needed for basic scrolling website
+// Add any custom JavaScript functionality here as needed for your design tech project
+  
